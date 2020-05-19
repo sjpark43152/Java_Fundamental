8d4f1add7c38e12adb8c1203098ba2af1b611c5f
sjpar43152 <sjpark43152@gmail.com>
2020-05-19 15:04:34 +0900

    2020-05-19-v1

--- src/java_20200519/Calendar.java
+++ src/java_20200519/Calendar.java
@@ -1,14 +1,17 @@
 package java_20200519;
 
 public class Calendar {
-	private int year;
-	private int month;
-	private int day;
+	private int[] monthArray = 
+		{31,28,31,30,31,30,31,31,30,31,30,31};
+	public static final int SUNDAY = 0;
+	public static final int MONDAY = 1;
+	public static final int TUESDAY = 2;
+	public static final int WEDNESDAY = 3;
+	public static final int THURSDAY = 4;
+	public static final int FRIDAY = 5;
+	public static final int SATURDAY = 6;
 	
-	public Calendar(int year, int month, int day) {
-		this.year = year;
-		this.month = month;
-		this.day = day;
+	public Calendar() {
 	}
 	private int getCount(int year, int month, int day) {
 		int totalCount = 0;
@@ -18,7 +21,6 @@ public class Calendar {
 		totalCount = preYear * 365 + 
 				(preYear/4 - preYear/100 + preYear/400);
 		
-		int[] monthArray = {31,28,31,30,31,30,31,31,30,31,30,31};
 		
 		//monthArray배열에서 year이 윤년인지 아닌지를 판단하여 윤년이면
 		//monthArray의 두번째 방의 값을 29로 변경해야 한다.
@@ -48,7 +50,7 @@ public class Calendar {
 		
 	}
 	
-	public void printCalendar(int year, int month) {
+	public void print(int year, int month) {
 		System.out.println("일\t월\t화\t수\t목\t금\t토");
 		
 		//2020년 5월1일 날짜를 구해서 일요일부터 1일 이전 날짜까지 빈공백 처리
@@ -70,48 +72,52 @@ public class Calendar {
 		}
 		System.out.println();
 	}
-	public void printCalendarYear(int year) {
+	public void print(int year) {
 		for(int i=1;i<=12;i++) {
-			printCalendar(year,i);
+			print(year,i);
 		}
 	}
 	
 	private int getLastDay(int year, int month) {
-        int[] monthArray = {31,28,31,30,31,30,31,31,30,31,30,31};
-		
+        
 		//monthArray배열에서 year이 윤년인지 아닌지를 판단하여 윤년이면
 		//monthArray의 두번째 방의 값을 29로 변경해야 한다.
 		
 		// 4의 배수는 윤년이고, 100의 배수는 윤년이 아니고, 400배수는 윤년이다.
-		boolean isLeafYear =
-				year%4==0 && 
-				year%100 != 0 || 
-				year%400==0;
+		boolean isLeafYear = isLeafYear(year);
 		
 		if(isLeafYear) {
 			monthArray[1] = 29;
 		}
 		return monthArray[month-1];
 	}
+	private boolean isLeafYear(int year) {
+		boolean isLeafYear =
+				year%4==0 && 
+				year%100 != 0 || 
+				year%400==0;
+		return isLeafYear;
+	}
 	
-	public void print() {
+	public void print(int year, int month, int day) {
 		
 		int totalCount = getCount(year, month, day);
-		int rest = totalCount % 7;
+		//dayOfWeek원리 주석
+		int dayOfWeek = totalCount % 7;
 		String message = null;
-		if(rest == 1) {
+		if(dayOfWeek == Calendar.MONDAY) {
 			message = "월요일";
-		}else if(rest == 2) {
+		}else if(dayOfWeek == Calendar.TUESDAY) {
 			message = "화요일";
-		}else if(rest == 3) {
+		}else if(dayOfWeek == Calendar.WEDNESDAY) {
 			message = "수요일";
-		}else if(rest == 4) {
+		}else if(dayOfWeek == Calendar.THURSDAY) {
 			message = "목요일";
-		}else if(rest == 5) {
+		}else if(dayOfWeek == Calendar.FRIDAY) {
 			message = "금요일";
-		}else if(rest == 6) {
+		}else if(dayOfWeek == Calendar.SATURDAY) {
 			message = "토요일";
-		}else if(rest == 0) {
+		}else if(dayOfWeek == Calendar.SUNDAY) {
 			message = "일요일";
 		}
 		
--- src/java_20200519/CalendarDemo.java
+++ src/java_20200519/CalendarDemo.java
@@ -1,11 +1,10 @@
 package java_20200519;
-
+//프린트 메소드 매우 중요.. 매우 신기함..
 public class CalendarDemo {
 	public static void main(String[] args) {
-		Calendar c = new Calendar(2020,6,25);
-		c.print();
-		c.printCalendar(2020,4);
-		c.printCalendarYear(2020);
+		Calendar c = new Calendar();
+		c.print(2019,5,12);
+		
 		
 	}
 
