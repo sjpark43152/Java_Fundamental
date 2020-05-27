fc39e71314a2ba36d3bcefe475d0e15135b2fd49
sjpar43152 <sjpark43152@gmail.com>
2020-05-27 12:16:06 +0900

    2020_05_27

--- src/java_20200527/CoinMarketCapCrawlingDemo.java
+++ src/java_20200527/CoinMarketCapCrawlingDemo.java
@@ -1,5 +1,6 @@
 package java_20200527;
 
+import java.io.FileWriter;
 import java.io.IOException;
 
 import org.jsoup.Jsoup;
@@ -10,46 +11,72 @@ import org.jsoup.select.Elements;
 public class CoinMarketCapCrawlingDemo {
 	
 	public static void main(String[] args) {
+		//개행쓸때 조금 불편"\n" 유닉스는 "\r" 맥에서 열면 개행안됨.
+		FileWriter fw = null;
+		
+		try {
+			fw = new FileWriter("c:\\dev\\coin.txt");
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		
+		
 		String url = "https://coinmarketcap.com/currencies/bitcoin/historical-data/?start=20200101&end=20200527";
 		
 		Document doc = null;
 		
 		try {
 			doc = Jsoup.connect(url).get();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			fw = new FileWriter("c:\\dev\\coin.txt");
+			
+		    Elements trElements = doc.select(".cmc-table__table-wrapper-outer table tbody tr");
 		
-		Elements trElements = doc.select(".cmc-table__table-wrapper-outer table tbody tr");
+		    System.out.println("날짜\t시가\t고가\t저가\t종가\t거래량\t총량");
+		    fw.write("날짜\t시가\t고가\t저가\t종가\t거래량\t총량\n");
 		
-		System.out.println("날짜\t시가\t고가\t저가\t종가\t거래량\t총량");
-		
+	/*	
 		for(int i=0;i<trElements.size();i++) {
 			Element trElement = trElements.get(i);
 			
 			Elements tdElements = trElement.select("td");
 			for(int j=0;j<tdElements.size();j++) {
 				Element tdElement = tdElements.get(j);
-				System.out.println(tdElement.text()+"\t");
+				System.out.print(tdElement.text()+"\t");
 			}
 		}
 		System.out.println();
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 	}
-
-}
+	*/
+	for(int i=0;i<trElements.size();i++) {
+		Element trElement = trElements.get(i);
+		String date = trElement.child(0).text();
+		String open = trElement.child(1).text();
+		String high = trElement.child(2).text();
+		String low = trElement.child(3).text();
+		String close = trElement.child(4).text();
+		String volume = trElement.child(5).text();
+		String marketCap = trElement.child(6).text();
+		System.out.println(date+"\t"+open+"\t"+high+"\t"+
+					low+"\t"+close+"\t"+volume+"\t"+marketCap);
+		
+		
+			fw.write(date+"\t"+open+"\t"+high+"\t"+
+					low+"\t"+close+"\t"+volume+"\t"+marketCap+"\n");
+		
+		}	 
+	}catch(IOException e) {
+		e.printStackTrace();
+		
+	}finally {
+		try{
+			if(fw !=null) fw.close();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+  } 
+	
+}	
+	
